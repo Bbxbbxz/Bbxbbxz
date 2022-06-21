a6433acc4d7fb679a3d1fc5798eff2192f4dcea2
4.16 <2910971081@qq.com>
2022-06-21 22:18:10 +0800

    docs

---
+++ docs/.vuepress/.temp/pages/index.html.vue
@@ -0,0 +1,8 @@
+<template><div><h1 id="hello-vuepress" tabindex="-1"><a class="header-anchor" href="#hello-vuepress" aria-hidden="true">#</a> Hello VuePress</h1>
+<p><RouterLink to="/">Home</RouterLink> <!-- 跳转到根部的 README.md -->
+<RouterLink to="/foo/">foo</RouterLink> <!-- 跳转到 foo 文件夹的 index.html -->
+<RouterLink to="/foo/#heading">foo heading anchor</RouterLink> <!-- 跳转到 foo/index.html 的特定 anchor 位置 -->
+<RouterLink to="/foo/one.html">foo - one</RouterLink> <!-- 具体文件可以使用 .html 结尾 -->
+<RouterLink to="/foo/two.html">foo - two</RouterLink> <!-- 也可以用 .md --></p>
+<p>🎉 💯 阿斌的文档</p>
+</div></template>
---
+++ docs/README.md
@@ -0,0 +1,11 @@
+---
+home: true
+tagline: 我是一只小小小小鸟~
+heroText: SHI-阿斌
+bgImageStyle: { height: "100vh" }
+heroTextStyle: {
+  color: '#fff'
+}
+bgImage: "/images/bg-logo.svg"
+isShowTitleInHome: false
+---
